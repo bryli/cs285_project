a199787fd899ff518cf2ed459764d19248407b80
Bryan Li <bryanli0@icloud.com>
2023-12-10 20:22:20 -0800

    Finished PPO runs

--- aerial_gym/envs/base/aerial_robot_with_vision.py
+++ aerial_gym/envs/base/aerial_robot_with_vision.py
@@ -326,7 +326,8 @@ class AerialRobotWithVision(BaseTask):
 
     def reset_idx(self, env_ids):
         num_resets = len(env_ids)
-        difficulty = 1#min(((self.counter // 4000 + 1) / 10.0), 1.0)
+        # difficulty = 1 # NO CURRICULUM LEARNING
+        difficulty = min(((self.counter // 4000 + 1) / 10.0), 1.0) # CURRICULUM LEARNING
         if 0 in env_ids:
             print(f"\n\n\n RESETTING ENV 0 (Difficulty {difficulty}) \n\n\n")
         self.env_asset_manager.randomize_pose(difficulty_factor=difficulty, heuristic=False)
