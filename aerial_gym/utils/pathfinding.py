6595f98bd94b6fbe3097e47b3f95ab6de362c5bf
Bryan Li <bryanli0@icloud.com>
2023-12-10 16:26:55 -0800

    Fixed pathfinding and added rudimentary curriculum learning

--- aerial_gym/utils/pathfinding.py
+++ aerial_gym/utils/pathfinding.py
@@ -1,4 +1,4 @@
-import torch
+import numpy as np
 
 class Node:
     def __init__(self, x, y):
@@ -7,32 +7,30 @@ class Node:
         self.parent = None
         self.cost = 0.0
 
-def rrt_star_path_length(obstacles, start=torch.tensor((5, 5), dtype=torch.float32), goal=torch.tensor((0, 0), dtype=torch.float32), max_iter=1000, step_size=1.0, min_radius=1.0):
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+def rrt_star_path_length(obstacles, start=np.array([5, 5]), goal=np.array([0, 0]), max_iter=10000, step_size=0.1, min_radius=0.5):
+    # device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-    start = start.to(device)
-    goal = goal.to(device)
-    obstacles = obstacles.to(device)
+    # start = start.to(device)
+    # goal = goal.to(device)
+    obstacles = obstacles.to("cpu").detach().numpy()
 
 
-    nodes = [Node(start[0].item(), start[1].item())]
+    nodes = [Node(start[0], start[1])]
     for _ in range(max_iter):
-        sample_node = Node(torch.rand(1) * 10.0, torch.rand(1) * 10.0)  # Adjust the range as needed
+        sample_node = Node(np.random.uniform(0, 10), np.random.uniform(0, 10))  # Adjust the range as needed
 
         # Find the nearest node in the tree
-        nearest_node = min(nodes, key=lambda n: torch.hypot(n.x - sample_node.x, n.y - sample_node.y))
+        nearest_node = min(nodes, key=lambda n: np.hypot(n.x - sample_node.x, n.y - sample_node.y))
 
         # Generate a new node in the direction of the sample
-        angle = torch.atan2(sample_node.y - nearest_node.y, sample_node.x - nearest_node.x)
-        new_x = nearest_node.x + step_size * torch.cos(angle)
-        new_y = nearest_node.y + step_size * torch.sin(angle)
-
-        new_x, new_y = new_x.to(device), new_y.to(device)
+        angle = np.arctan2(sample_node.y - nearest_node.y, sample_node.x - nearest_node.x)
+        new_x = nearest_node.x + step_size * np.cos(angle)
+        new_y = nearest_node.y + step_size * np.sin(angle)
 
         # Check for collisions within the specified radius
         collision = False
         for obstacle in obstacles:
-            distance = torch.hypot(new_x - obstacle[0], new_y - obstacle[1])
+            distance = np.hypot(new_x - obstacle[0], new_y - obstacle[1])
             if distance < min_radius:
                 collision = True
                 break
@@ -44,17 +42,17 @@ def rrt_star_path_length(obstacles, start=torch.tensor((5, 5), dtype=torch.float
 
             # Update the cost of nearby nodes if a lower-cost path is found
             for node in nodes:
-                if torch.hypot(node.x - new_node.x, node.y - new_node.y) < min_radius and node.cost > new_node.cost:
+                if np.hypot(node.x - new_node.x, node.y - new_node.y) < min_radius and node.cost > new_node.cost:
                     node.parent = new_node
                     node.cost = new_node.cost
 
             nodes.append(new_node)
 
             # Check if the goal is reached
-            if torch.hypot(new_x - goal[0], new_y - goal[1]) < min_radius:
+            if np.hypot(new_x - goal[0], new_y - goal[1]) < min_radius:
                 goal_node = Node(goal[0].item(), goal[1].item())
                 goal_node.parent = new_node
-                goal_node.cost = new_node.cost + torch.hypot(new_x - goal[0], new_y - goal[1])
+                goal_node.cost = new_node.cost + np.hypot(new_x - goal[0], new_y - goal[1])
                 nodes.append(goal_node)
 
                 # Reconstruct the path
