30d56ec92b8dd90bb459f5ebf68c7c7ec1df3b16
Bryan Li <bryanli0@icloud.com>
2023-12-10 19:39:32 -0800

    Increased number of environments to 256 and fixed difficulty at 1 (no curriculum learning)

--- aerial_gym/envs/base/aerial_robot_with_vision.py
+++ aerial_gym/envs/base/aerial_robot_with_vision.py
@@ -326,10 +326,11 @@ class AerialRobotWithVision(BaseTask):
 
     def reset_idx(self, env_ids):
         num_resets = len(env_ids)
-        difficulty = min(((self.counter // 4000 + 1) / 10.0), 1.0)
+        difficulty = 1#min(((self.counter // 4000 + 1) / 10.0), 1.0)
         if 0 in env_ids:
             print(f"\n\n\n RESETTING ENV 0 (Difficulty {difficulty}) \n\n\n")
         self.env_asset_manager.randomize_pose(difficulty_factor=difficulty, heuristic=False)
+        # self.env_asset_manager.randomize_pose(heuristic=False)
         # self.env_asset_manager.randomize_pose(difficulty_factor=min(((self.counter // 100000 + 1) / 10.0), 1.0), heuristic=False)
         
         self.env_asset_root_states[env_ids, :, 0:3] = self.env_asset_manager.asset_pose_tensor[env_ids, :, 0:3]
